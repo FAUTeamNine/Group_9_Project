76f3f8cc8cbe571d2c10f7f93706f66409281b8a
gflorestal2018 <77472969+gflorestal2018@users.noreply.github.com>
2022-11-19 14:44:46 -0500

    login screen progress and alamo fire image addition

---
+++ Rainy/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Rainy
+//
+//  Created by Garry Florestal on 11/19/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
